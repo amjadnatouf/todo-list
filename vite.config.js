319c438757a49e6e8f99564e0fb1c974b4143c1c
Amjad Natouf <amna2300@student.miun.se>
2024-02-28 17:11:38 +0100

    reset-depolyment-options

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   base: "/todos/",
-  plugins: [
-    // other Vite plugins
-    react({
-      babel: {
-        plugins: [
-          // other Babel plugins
-          [
-            "@locator/babel-jsx/dist",
-            {
-              env: "development",
-            },
-          ],
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
 });
